0b32e398c8cce8d40440074ada87eea479325bd3
jublar.garcia <jublar@gmail.com>
2019-08-29 13:36:57 -0500

    Adding constants class for msgs

---
+++ kenect-spring-test-1/src/main/java/com/kenect/kenectspringtest/constants/Constants.java
@@ -0,0 +1,30 @@
+package com.kenect.kenectspringtest.constants;
+
+/**
+ * <p>Constants class.</p>
+ *
+ * @author Jublar Garcia
+ * @version 1.0
+ */
+public class Constants {
+    /** Constant <code>MSG_NO_ELEMENT="There is no element with id: %d"</code> */
+    public static final String MSG_NO_ELEMENT = "There is no element with id: %d.";
+    /** Constant <code>MSG_ELEMENT_DOES_NOT_EXISTS="%s with id %d does not exists."</code> */
+    public static final String MSG_ELEMENT_DOES_NOT_EXISTS = "%s with id %d does not exists.";
+    /** Constant <code>MSG_EMAIL_OBJECT_REQUIRED="Email object is required."</code> */
+    public static final String MSG_EMAIL_OBJECT_REQUIRED = "Email object is required.";
+    /** Constant <code>MSG_EMAIL_REQUIRED="Email is required."</code> */
+    public static final String MSG_EMAIL_REQUIRED = "Email is required.";
+    /** Constant <code>MSG_EMAIL_FORMAT="Email format not correct."</code> */
+    public static final String MSG_EMAIL_FORMAT = "Email format not correct.";
+    /** Constant <code>MSG_ADDRESS_OBJECT_REQUIRED="Address object is required."</code> */
+    public static final String MSG_ADDRESS_OBJECT_REQUIRED = "Address object is required.";
+    /** Constant <code>MSG_STREET_REQUIRED="Street is required."</code> */
+    public static final String MSG_STREET_REQUIRED = "Street is required.";
+    /** Constant <code>MSG_PHONE_OBJECT_REQUIRED="Phone object is required."</code> */
+    public static final String MSG_PHONE_OBJECT_REQUIRED = "Phone object is required.";
+    /** Constant <code>MSG_NUMBER_OBJECT_REQUIRED="Phone number is required."</code> */
+    public static final String MSG_NUMBER_OBJECT_REQUIRED = "Phone number is required.";
+    /** Constant <code>MSG_NUMBER_FORMAT="Phone number format not correct."</code> */
+    public static final String MSG_NUMBER_FORMAT = "Phone number format not correct.";
+}
